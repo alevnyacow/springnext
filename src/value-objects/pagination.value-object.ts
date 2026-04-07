1c5c854d52d3672e3b7c22fb5c01db85a63df8ed
alevny <alevnyacow@gmail.com>
2026-04-07 19:27:52 +0400

    feat: docs

--- src/value-objects/pagination.value-object.ts
+++ src/value-objects/pagination.value-object.ts
@@ -2,9 +2,18 @@ import z from 'zod';
 
 export type PaginationModel = z.infer<typeof Pagination.schema>;
 
+/**
+ * Pagination value object. Used in store `list` method.
+ */
 export class Pagination {
     static schema = z.object({
+        /**
+         * Zero based page index. Can't be negative.
+         */
         zeroBasedIndex: z.coerce.number().int().nonnegative(),
+        /**
+         * Page size. Must be positive.
+         */
         pageSize: z.coerce.number().int().positive()
     });
 
@@ -19,6 +28,9 @@ export class Pagination {
         return this.data;
     }
 
+    /**
+     * Checks if two Pagination objects are same.
+     */
     same = (pagination: Pagination) => {
         return (
             pagination.model.pageSize === this.model.pageSize &&
@@ -26,6 +38,9 @@ export class Pagination {
         );
     };
 
+    /**
+     * Obtains next page.
+     */
     get nextPage() {
         return Pagination.create({
             pageSize: this.data.pageSize,
@@ -34,7 +49,7 @@ export class Pagination {
     }
 
     /**
-     * Safe implementation. If it's first page, pagination with the same model is returned.
+     * Obtains previous page. Safe implementation - if it's first page, pagination with the same model is returned.
      */
     get previousPage() {
         if (this.model.zeroBasedIndex === 0) {
@@ -47,6 +62,9 @@ export class Pagination {
         });
     }
 
+    /**
+     * Obtain previous page. Unsafe implementation.
+     */
     get previousPage_UNSAFE() {
         return Pagination.create({
             pageSize: this.model.pageSize,
