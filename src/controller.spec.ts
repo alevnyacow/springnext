8b593e0a2df3d8e5d0da53c3074f38d87e289322
alevny <alevnyacow@gmail.com>
2026-04-07 16:46:08 +0400

    added POST test

--- src/controller.spec.ts
+++ src/controller.spec.ts
@@ -10,6 +10,11 @@ const testControllerEndpoints = endpoints({
         GET: { 
             query: z.object({ id: z.string() }),
             response: z.object({ idFromQuery: z.string() })
+        },
+        POST: {
+            body: z.object({ bodyParam: z.string() }),
+            query: z.object({ queryParam: z.string() }),
+            response: z.object({ paramsJoinedWithSpace: z.string() })
         }
     }
 })
@@ -18,10 +23,22 @@ const GET = testControllerEndpoints('GET', async ({ id }) => {
     return { idFromQuery: id }
 })
 
-
 test('GET test', async () => {
     const data = await GET(new NextRequest('http://localhost.mock.url:3000?id=hello'))
     const responseBody = await data.json() as { idFromQuery: string }
     expect(responseBody.idFromQuery).toBe('hello')
 })
 
+const POST = testControllerEndpoints('POST', async ({ bodyParam, queryParam }) => ({ 
+    paramsJoinedWithSpace: `${bodyParam} ${queryParam}` }
+))
+
+test('POST test', async () => {
+    const data = await POST(new NextRequest('http://localhost.mock.url:3000?queryParam=query', {
+        body: JSON.stringify({ bodyParam: 'body' }),
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+    }))
+    const responseBody = await data.json() as { paramsJoinedWithSpace: string }
+    expect(responseBody.paramsJoinedWithSpace).toBe('body query')
+})
